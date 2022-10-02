cfabf576cc813d315f6f51dd6ec46b27843474f3
PoProstuMichal89 <michal.andrzej.mazur89@gmail.com>
2022-10-03 01:54:37 +0200

    edycja i dodanie nowych danych testowych

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -63,7 +63,13 @@ VALUES
     (4, 2),
     (4, 3),
     (4, 5),
-    (4, 6);
+    (4, 6),
+    (3, 2),
+    (3, 1),
+    (3, 5),
+    (2, 2),
+    (2, 1),
+    (2, 4);
 
 INSERT INTO
 opinions(content, nickname, rate, drink_id)
